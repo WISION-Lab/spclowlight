bde3ff6ae206dd926d0d422b174e9e1b5d4d6d7f
Bhavya Goyal <bhavyagoyal777@gmail.com>
2021-03-23 21:32:03 -0500

    Updted

--- simulation/create_data.py
+++ simulation/create_data.py
@@ -6,13 +6,7 @@ BASE = '/srv/home/bhavya/datasets/imagenet/'
 # Generated Images for SPC
 BASENEW = BASE.replace('imagenet', 'imagenet_generated/average60')
 # number of frames to average
-frames = [4, 16, 64, 256]
-#frames = [4, 8, 16, 32, 64, 128]
-#frames = [16, 32, 64, 128, 256, 9, 84]
-#frames = [17, 52, 90, 80, 160, 90, 32, 21, 46, 18, 56]
-#frames = [3, 4, 6, 7, 8, 14, 20, 28, 40, 42, 85, 120, 170, 240]
-#frames = [2,3,4,5]
-#frames = [1]
+frames = [1, 4, 16, 64, 256]
 
 # Settings for MiniPaces and COCO dataset
 #BASE = '/srv/home/bhavya/datasets/miniplaces2/data/'
@@ -45,15 +39,6 @@ for line in im_list[start:end]:
     count_shape = im.shape + (-1,)
     
     for i in range(1):
-        exists=True
-        for fil in frames:
-            outfile = BASENEW.replace('60', str(fil)) + str(i) + im_name.replace('JPEG', 'png')
-            if(not os.path.exists(outfile)):
-                exists = False
-        if(exists):
-            print('Skipping ', im_name)
-            continue
-
         # Simulate low light conditions by scaling pixel values by 1/1000.
         # Poisson process to simulate photon arrival
         photon_counts = np.random.poisson(im.flatten()/1000.0, size=(TOTAL_FRAMES, im.size)).T
@@ -62,10 +47,8 @@ for line in im_list[start:end]:
         b_counts=np.where(photon_counts>=1, 1, 0)
         for fil in frames:
             recon_image = np.mean(b_counts[:,:,:,0:fil], axis=3)
-            #recon_image = im/1000.
             #mn += np.mean(recon_image, axis=(0,1))
             #mn2 += np.mean(recon_image**2, axis=(0,1))
-            #outfile = BASENEW + im_name.replace('jpg', 'png')
             outfile = BASENEW.replace('60', str(fil)) + str(i) + im_name.replace('JPEG', 'png')
             #outfile = BASENEW.replace('60', str(fil)) + 'images/' + str(i) + '/' + im_name[7:].replace('jpg', 'png')
             #print(outfile)
